bd89a1117be9a665a796861a8536c33b5c4c8623
Ashish Khawla <khawla@fanaticcoders.com>
2026-04-10 09:57:32 +0530

    feat: add ItinerarySubmittedAdminMail mailable and template

---
+++ app/Mail/ItinerarySubmittedAdminMail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Itinerary;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ItinerarySubmittedAdminMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $itinerary;
+    public $creator;
+
+    public function __construct(Itinerary $itinerary, User $creator)
+    {
+        $this->itinerary = $itinerary;
+        $this->creator = $creator;
+    }
+
+    public function build()
+    {
+        return $this->subject('New Itinerary Awaiting Approval')
+                    ->view('emails.itinerary-submitted-admin', [
+                        'itinerary' => $this->itinerary,
+                        'creator' => $this->creator,
+                    ]);
+    }
+}
